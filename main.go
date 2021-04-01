15275fddb7fc8f1830327e8445653174ff0f6e44
dusan <dusan.kuburic@outlook.com>
2021-04-01 14:38:26 +0200

    crud done

--- main.go
+++ main.go
@@ -15,23 +15,25 @@ import (
 )
 
 type Book struct {
-	ID     int    `json:id`
-	Title  string `json:title`
-	Author string `json:author`
-	Year   string `json:year`
-}
-
-var books []Book
-var db *sql.DB
-
-func init() {
-	gotenv.Load()
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
 }
 
 func logFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+
+var books []Book
+var db *sql.DB
+
+func init() {
+	err := gotenv.Load()
+	logFatal(err)
 }
 
 func main() {
@@ -89,13 +91,39 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	var bookID int
 
+	json.NewDecoder(r.Body).Decode(&book)
+
+	err := db.QueryRow("insert into books(title, author, year) values ($1, $2, $3) RETURNING id;",
+		book.Title, book.Author, book.Year).Scan(&bookID)
+
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(bookID)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
 
+	result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id",
+		&book.Title, &book.Author, &book.Year, &book.ID)
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	result, err := db.Exec("delete from books where id = $1", params["id"])
+	logFatal(err)
+
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }

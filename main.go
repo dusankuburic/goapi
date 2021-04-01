bbfd003cfc56657a79a2acbb17ac17f325a7c3fc
dusan <dusan.kuburic@outlook.com>
2021-04-01 02:39:47 +0200

    endpoint implemented

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Book struct {
-	ID     string `json:id`
+	ID     int    `json:id`
 	Title  string `json:title`
 	Author string `json:author`
 	Year   string `json:year`
@@ -16,8 +18,14 @@ type Book struct {
 var books []Book
 
 func main() {
-
 	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: 3, Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
 
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
@@ -29,24 +37,51 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get all books")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get one book")
+	params := mux.Vars(r)
+
+	id, _ := strconv.Atoi(params["id"])
+
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Adds one book")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(books)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Updates a book")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Remobes a book")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for i, item := range books {
+		if item.ID == id {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
-
-
-
